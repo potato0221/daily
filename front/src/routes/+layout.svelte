d21a72541467ff7ade8154a70c911f4aca86151c
potato0221 <wnqo10@naver.com>
2024-04-24 00:05:07 +0900

    feat: 프론트 셋팅

---
+++ front/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '$lib/app.css';
+	const { children } = $props();
+</script>
+
+<header class="navbar bg-gray-50 shadow">
+	<div class="flex justify-center">daily</div>
+</header>
+<main class="m-2">{@render children()}</main>
+<footer class=" bottom-0 w-full bg-white text-gray-300 m-2">푸터</footer>
